113c58451ace5b0e6ede08733797e0d27d7a26f2
Ethan White <ethan@weecology.org>
2016-11-15 15:32:30 -0500

    Add function to determine size class
    Split species into large and small size classes with 50 g determining the size category.

--- large-small-ts-analysis.R
+++ large-small-ts-analysis.R
@@ -1,3 +1,12 @@
 get_data <- function(){
   data <- read.csv("surveys.csv")
 }
+
+get_size_class <- function(weight){
+  if (weight > 50){
+    size_class = "large"
+  } else {
+    size_class = "small"
+  }
+  return(size_class)
+}
