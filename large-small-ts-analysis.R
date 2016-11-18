05f807aa2d7be440d56130d7a058e1267fe9fc06
Christine Swanson <christineswanson@ufl.edu>
2016-11-18 12:51:12 -0500

    Add function for getting size class series data

--- large-small-ts-analysis.R
+++ large-small-ts-analysis.R
@@ -10,3 +10,14 @@ get_size_class <- function(weight, threshold){
   }
   return(size_class)
 }
+
+get_size_class_ts_data <- function(df){
+  # Convert individual data to time-series data for each of a set of size classes
+  # Input: data frame with a year column for time
+  #        and a size_class column
+  ts_data <-
+    df %>% 
+    group_by(year, size_class) %>% 
+    summarize(counts = n())
+  return(ts_data)
+}
